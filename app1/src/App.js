6da55ac4912aee5b7c027348c63d3bb44f9833c8
NirapatJK <s6503051624190@email.kmutnb.ac.th>
2025-07-14 11:41:31 +0700

    update style
    update style css

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
 import React from 'react'
+import './style.css'
 import './App.css';
 
 function App() {
+  return (
+    <>
+    <h2>DeveloperThai.com</h2>
+    <div className='title'>React & React Native</div>
+    </>
+  )
   const divStyle = {
     color: 'red',
     backgroundColor: 'powderblue',
