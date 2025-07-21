b19c36cdf49c233a0897a2817637c411f7d84393
NirapatJK <s6503051624190@email.kmutnb.ac.th>
2025-07-21 11:33:12 +0700

    class-components update
    update getDate()

--- app1/src/class-components.js
+++ app1/src/class-components.js
@@ -1,7 +1,5 @@
-/*
-import React,{Component}from 'react'
-
-export default class Calendar extends {
+import React, {Component} from 'react'
+export default class Calendar extends Component{
   getDate(){
      const dayNames = ['อาทิตย์','จันทร์','อังคาร','พุธ','พฤหัสบดี','ศุกร์','เสาร์']
      const monthNames = ['มกราคม','กุมภาพันธ์','มีนาคม','เมษายน','พฤษภาคม','มิถุนายน','กรกฎาคม','สิงหาคม','กันยายน','ตุลาคม','พฤศจิกายน','ธันวาคม']
@@ -12,9 +10,9 @@ export default class Calendar extends {
      const year = date.getFullYear() + 543
      return `วัน${weekDay} ที่ ${day} ${month} ${year}`
   } 
-  render() {
-    return <div>{}</div>   
+  render(){
+    return( 
+      <div>{this.getDate()}</div>
+    )
   }
- 
 }
-*/
