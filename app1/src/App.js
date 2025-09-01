75723ac703ebcb54651b707456f61dbb3c62a5fd
NirapatJK <s6503051624190@email.kmutnb.ac.th>
2025-09-01 10:55:21 +0700

    Lab9
    Lab9-test

--- app1/src/App.js
+++ app1/src/App.js
@@ -4,17 +4,31 @@
 //import{Header,Content,Footer} from './func-components'
 //import Button from './class-components'
 //import MessageBox from './state-func'
-import React from 'react'
-import {userContext} from './context'
-import Header2 from './context-header2'
-import Content2 from './context-content2'
+//import {userContext} from './context'
+//import Header2 from './context-header2'
+//import Content2 from './context-content2'
+//import React from 'react'
+import {createContext, useState} from 'react'
+import Menu from "./component/Menu";
+import Quiz from "./component/Quiz";
+import Score from "./component/Score";
+import './App.css'
 
-export default function App(){
-  let [user, setUser] = React.useState('')
+export const DataContext = createContext()
+
+function App() {
+  const [appState, setAppState] = useState("menu")
+  const [score, setScore] = useState(0)
   return (
-  <userContext.Provider value= {[user,setUser]} >
-    <Header2/>
-    <Content2/>
-  </userContext.Provider>
-  )
+    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
+      <div className ="App">
+        <h1>Web Development Quiz</h1>
+        {appState === "menu" && <Menu/>}
+        {appState === "quiz" && <Quiz/>}
+        {appState === "score" && <Score/>}
+      </div>
+    </DataContext.Provider>
+  );
 }
+
+export default App;
