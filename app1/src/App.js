6901e47cb96730256d5c04464a63bd1841fbbd2b
NirapatJK <s6503051624190@email.kmutnb.ac.th>
2025-09-08 10:05:34 +0700

    Add NavLink
    Add Router2

--- app1/src/App.js
+++ app1/src/App.js
@@ -7,28 +7,10 @@
 //import {userContext} from './context'
 //import Header2 from './context-header2'
 //import Content2 from './context-content2'
-//import React from 'react'
-import {createContext, useState} from 'react'
-import Menu from "./component/Menu";
-import Quiz from "./component/Quiz";
-import Score from "./component/Score";
-import './App.css'
 
-export const DataContext = createContext()
+import React from 'react'
+import Router2 from './router2';
 
-function App() {
-  const [appState, setAppState] = useState("menu")
-  const [score, setScore] = useState(0)
-  return (
-    <DataContext.Provider value={{appState,setAppState,score,setScore}}>
-      <div className ="App">
-        <h1>Web Development Quiz</h1>
-        {appState === "menu" && <Menu/>}
-        {appState === "quiz" && <Quiz/>}
-        {appState === "score" && <Score/>}
-      </div>
-    </DataContext.Provider>
-  );
+export default function App(){
+  return <Router2/>
 }
-
-export default App;
