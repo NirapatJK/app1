d6dd3b2b718f7a47878cab91e2894922b24bf574
NirapatJK <s6503051624190@email.kmutnb.ac.th>
2025-09-08 10:54:08 +0700

    Add NavLink2
    Add Router3

--- app1/src/App.js
+++ app1/src/App.js
@@ -9,8 +9,8 @@
 //import Content2 from './context-content2'
 
 import React from 'react'
-import Router2 from './router2';
+import Router3 from './router3';
 
 export default function App(){
-  return <Router2/>
+  return <Router3/>
 }
