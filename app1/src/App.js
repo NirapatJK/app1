6b19a9c12c85e2626fca6568fc90503ee3faa73c
NirapatJK <s6503051624190@email.kmutnb.ac.th>
2025-09-15 11:07:32 +0700

    FormSelectFile add
    FormSelectFile update

--- app1/src/App.js
+++ app1/src/App.js
@@ -9,8 +9,8 @@
 //import Content2 from './context-content2'
 
 import React from 'react'
-import FormText from './form-text';
+import FormSelectFile from './form-select-file';
 
 export default function App(){
-  return <FormText/>
+  return <FormSelectFile/>
 }
